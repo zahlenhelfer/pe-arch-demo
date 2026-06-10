0ba673ee20afb874fd69cd8b268498b73e77aea8
Marcus Ross <marcus.ross@zahlenhelfer.de>
2026-06-10 14:19:11 +0200

    added url for testing

--- bootstrap/01-monitoring-stack/01-test-grafana.sh
+++ bootstrap/01-monitoring-stack/01-test-grafana.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+echo "========================================"
+echo "Testing Grafana"
+echo "========================================"
+echo "use your browser to navigate to:"
+echo "http://grafana.172.18.255.254.sslip.io"
+echo "========================================"
+echo "Testing Grafana via curl"
 curl --fail --silent --show-error http://grafana.172.18.255.254.sslip.io
